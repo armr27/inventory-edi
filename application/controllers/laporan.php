9b6ef4bc509bb81cffe2cd1fc4e72df1fdb84734
armr27 <ariamri89@gmail.com>
2022-12-15 00:14:08 +0700

    Update laporan barang masuk dan hapus mpdf

--- application/controllers/laporan.php
+++ application/controllers/laporan.php
@@ -1,68 +1,60 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Laporan extends CI_Controller {
+class Laporan extends CI_Controller
+{
 
-    public function __construct() {
-        parent::__construct();
-        $this->load->helper('url');
-        $this->load->helper('download');
-        $this->load->library('pagination');
-        $this->load->helper('cookie');
-        $this->load->model('barangMasuk_model');
-        $this->load->model('barangKeluar_model');
-      }
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->helper('url');
+    $this->load->helper('download');
+    $this->load->library('pagination');
+    $this->load->helper('cookie');
+    $this->load->model('barangMasuk_model');
+    $this->load->model('barangKeluar_model');
+  }
 
-    public function barang_masuk_pdf()
-    {
-      $tglawal = $this->input->post('tglawal');
-      $tglakhir = $this->input->post('tglakhir');
+  public function barang_masuk_pdf()
+  {
+    $tglawal = $this->input->post('tglawal');
+    $tglakhir = $this->input->post('tglakhir');
 
-      if($tglawal != '' && $tglakhir != ''){
-        $data['data'] = $this->barangMasuk_model->lapdata($tglawal, $tglakhir)->result();
-      }
-      else{
-        $data['data'] = $this->barangMasuk_model->dataJoin()->result();
-      }
-
-      $data['tglawal'] = $tglawal;
-      $data['tglakhir'] = $tglakhir;
-
-      $data['judul'] = 'Laporan Barang Masuk';
-      $mpdf = new \Mpdf\Mpdf();
-      $html = $this->load->view('laporan/barang_masuk_pdf',$data,true);
-      $mpdf->WriteHTML($html);
-      $tgl = date('Ymd_his');
-      $namaFile = 'Barang_masuk_'.$tgl.'.pdf';
-      $mpdf->Output($namaFile, 'D');
-
+    if ($tglawal != '' && $tglakhir != '') {
+      $data['data'] = $this->barangMasuk_model->lapdata($tglawal, $tglakhir)->result();
+    } else {
+      $data['data'] = $this->barangMasuk_model->dataJoin()->result();
     }
 
-    public function barang_keluar_pdf()
-    {
-      $tglawal = $this->input->post('tglawal');
-      $tglakhir = $this->input->post('tglakhir');
+    $data['tglawal'] = $tglawal;
+    $data['tglakhir'] = $tglakhir;
 
-      if($tglawal != '' && $tglakhir != ''){
-        $data['data'] = $this->barangKeluar_model->lapdata($tglawal, $tglakhir)->result();
-      }
-      else{
-        $data['data'] = $this->barangKeluar_model->dataJoin()->result();
-      }
+    $data['judul'] = 'Laporan Barang Masuk';
+    $this->load->view('laporan/barang_masuk_pdf', $data);
+    $tgl = date('Ymd_his');
+    $namaFile = 'Barang_masuk_' . $tgl . '.pdf';
+  }
 
-      $data['tglawal'] = $tglawal;
-      $data['tglakhir'] = $tglakhir;
+  public function barang_keluar_pdf()
+  {
+    $tglawal = $this->input->post('tglawal');
+    $tglakhir = $this->input->post('tglakhir');
 
-      $data['judul'] = 'Laporan Barang Keluar';
-      $mpdf = new \Mpdf\Mpdf();
-      $html = $this->load->view('laporan/barang_keluar_pdf',$data,true);
-      $mpdf->WriteHTML($html);
-      $tgl = date('Ymd_his');
-      $namaFile = 'Barang_keluar_'.$tgl.'.pdf';
-      $mpdf->Output($namaFile, 'D');
-
+    if ($tglawal != '' && $tglakhir != '') {
+      $data['data'] = $this->barangKeluar_model->lapdata($tglawal, $tglakhir)->result();
+    } else {
+      $data['data'] = $this->barangKeluar_model->dataJoin()->result();
     }
 
+    $data['tglawal'] = $tglawal;
+    $data['tglakhir'] = $tglakhir;
 
-
+    $data['judul'] = 'Laporan Barang Keluar';
+    $mpdf = new \Mpdf\Mpdf();
+    $html = $this->load->view('laporan/barang_keluar_pdf', $data, true);
+    $mpdf->WriteHTML($html);
+    $tgl = date('Ymd_his');
+    $namaFile = 'Barang_keluar_' . $tgl . '.pdf';
+    $mpdf->Output($namaFile, 'D');
+  }
 }
--- application/views/laporan/barang_masuk_pdf.php
+++ application/views/laporan/barang_masuk_pdf.php
@@ -1,105 +1,213 @@
 <?php
-function tgl_indo($tanggal){
-	$bulan = array (
-		1 =>   'Januari',
-		'Februari',
-		'Maret',
-		'April',
-		'Mei',
-		'Juni',
-		'Juli',
-		'Agustus',
-		'September',
-		'Oktober',
-		'November',
-		'Desember'
-	);
-	$pecahkan = explode('-', $tanggal);
-	
-	// variabel pecahkan 0 = tanggal
-	// variabel pecahkan 1 = bulan
-	// variabel pecahkan 2 = tahun
- 
-	return $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0];
+function tgl_indo($tanggal)
+{
+  $bulan = array(
+    1 =>   'Januari',
+    'Februari',
+    'Maret',
+    'April',
+    'Mei',
+    'Juni',
+    'Juli',
+    'Agustus',
+    'September',
+    'Oktober',
+    'November',
+    'Desember'
+  );
+  $pecahkan = explode('-', $tanggal);
+
+  // variabel pecahkan 0 = tanggal
+  // variabel pecahkan 1 = bulan
+  // variabel pecahkan 2 = tahun
+
+  return $pecahkan[2] . ' ' . $bulan[(int)$pecahkan[1]] . ' ' . $pecahkan[0];
 }
 ?>
 
-<!DOCTYPE html>
 <html>
+
 <head>
-    <title><?= $judul ?></title>
-<style>
+  <title>Sinarmas Cepsa</title>
+  <style type="text/css">
+    <!--
+    .style1 {
+      font-size: 14px;
+      font-weight: bold;
+    }
 
-body{
-    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-}
-#customers {
-  border-collapse: collapse;
-  width: 100%;
-  
-}
+    .style2 {
+      font-size: 14px
+    }
 
-#customers td {
-  border: 0px solid $#ddd;
-  padding: 8px;
-  font-size: 12px;
-}
-#customers th{
-  padding: 8px;
-  font-size: 12px;
-}
+    .style4 {
+      font-size: 12px;
+      font-weight: bold;
+    }
 
+    .style6 {
+      font-size: 24px;
+      font-weight: bold;
+    }
+    -->
+    .keliling
+    {
+    border-collapse:collapse;
+    font-size:12px;
+    }
+    .keliling
+    tr
+    td
+    {
+    border:1px
+    #000
+    solid;
+    }
+    .atas
+    {
+    font-size:12px;
+    }
+    .bawah
+    {
+    font-size:12px;
+    }
 
-#customers tr:nth-child(even){background-color: #f2f2f2;}
-
-#customers tr:hover {background-color: #ddd;}
-
-#customers th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #858796;
-  color: white;
-}
-</style>
+  </style>
 </head>
-<body>
-<table border="0" width="100%">
-    <tr>
-        <td align="center"><h1>Laporan Barang Masuk</h1></td>
-    </tr>
-    <tr>
-        <td align="center">
-            <?php if($tglawal == '' || $tglakhir == ''): ?>
-                <h6>Semua Tanggal</h6>
-            <?php else: ?>
-                <h6><?= tgl_indo($tglawal) ?> - <?= tgl_indo($tglakhir) ?></h6>
-            <?php endif; ?>
-            
-        </td>
-    </tr>
-</table>
-<br>
-<table id="customers">
-  <tr>
-    <th>No</th>
-    <th>Tanggal Masuk</th>
-    <th>No.Transaksi</th>
-    <th>Supplier</th>
-    <th>Nama Barang</th>
-    <th>Jumlah Masuk</th>
-  </tr>
-      <?php $no=1; foreach ($data as $d) { ?>
-        <tr>
-          <td><?= $no++ ?></td>
-          <td><?= tgl_indo($d->tgl_masuk) ?></td>
-          <td><?= $d->id_barang_masuk ?></td>
-          <td><?= $d->nama_supplier ?></td>
-          <td><?= $d->nama_barang ?></td>
-          <td><?= $d->jumlah_masuk ?></td>
-        </tr>
-      <?php } ?>
-</table>
 
+<body>
+
+  <form action="<?php $PHP_SELF; ?>" method="post" name="form1" target="_self">
+    <table width="750" border="1" align="center" cellpadding="0" cellspacing="1">
+
+
+      <tr class="brs_isi">
+        <td valign="top">
+          <br>
+          <br>
+          <table width="650" border="0" align="center" cellpadding="2" cellspacing="1">
+
+            <!-- <tr align="center">
+                            <td width="67" rowspan="4" align="left">
+                                <div align="center">
+                                </div>
+                                <div align="left"></div>
+                                <div align="center" class="style1"></div>
+                                <div align="left">
+                                    <input name="imageField" type="image" src="logo.png" width="80" height="80">
+                                </div>
+                            </td>
+                            <td align="left">
+                                <div align="center" class="style4">YAYASAN PENDIDIKAN MAKMUR RIDAR (YPMR)</div>
+                            </td>
+                            <td align="left">&nbsp;</td>
+                        </tr>
+                        <tr align="center">
+                            <td width="503" align="left">
+                                <div align="center"><span class="style1">SEKOLAH TINGGI MANAJEMEN INFORMATIKA DAN KOMPUTER</span></div>
+                            </td>
+                            <td width="69" align="left">&nbsp;</td>
+                        </tr>
+                        <tr align="center">
+                            <td align="left">
+                                <div align="center"><span class="style6">[STMIK] DUMAI </span></div>
+                            </td>
+                            <td align="left">&nbsp;</td>
+                        </tr>
+                        <tr align="center">
+                            <td align="left">
+                                <div align="center">Jl. Sungai Rokan No. 95 Dumai, Riau, Indonesia 28814</div>
+                            </td>
+                            <td align="left">&nbsp;</td>
+                        </tr> -->
+            <!-- <tr align="center">
+                            <td colspan="3" align="left">
+                                <div align="left">
+                                    <hr width="650" size="1px solid" noshade color="#CC6600">
+                                </div>
+                            </td>
+                        </tr>
+                        <tr align="center">
+                            <td colspan="3" align="left">
+                                <div align="left"></div>
+                            </td>
+                        </tr> -->
+            <tr align="center">
+              <td colspan="3" align="left">
+                <div align="center" class="style1 style2">LAPORAN BARANG MASUK SINARMAS CEPSA</div>
+                <div align="left"></div>
+              </td>
+            </tr>
+            <tr>
+              <td align="center">
+                <?php if ($tglawal == '' || $tglakhir == '') : ?>
+                  <h6>Semua Tanggal</h6>
+                <?php else : ?>
+                  <h6><?= tgl_indo($tglawal) ?> - <?= tgl_indo($tglakhir) ?></h6>
+                <?php endif; ?>
+
+              </td>
+            </tr>
+          </table>
+          <br>
+
+          <table width="650" border="0" align="center" cellpadding="2" cellspacing="1" class="keliling">
+            <thead>
+              <tr class="batas2" align="center">
+                <td rowspan="2" width="2%"><b>NO.</b></td>
+                <td rowspan="2" width="6%"><b>Tanggal Masuk</b></td>
+                <td rowspan="2" width="6%"><b>No. Transaksi</b></td>
+                <td rowspan="2" width="6%"><b>Suplier</b></td>
+                <td rowspan="2" width="6%"><b>Nama Barang</b></td>
+                <td rowspan="2" width="6%"><b>Jumlah Masuk</b></td>
+              </tr>
+            </thead>
+            <tbody>
+              <?php $no = 1;
+              foreach ($data as $d) { ?>
+                <tr>
+                  <td><?= $no++ ?></td>
+                  <td><?= tgl_indo($d->tgl_masuk) ?></td>
+                  <td><?= $d->id_barang_masuk ?></td>
+                  <td><?= $d->nama_supplier ?></td>
+                  <td><?= $d->nama_barang ?></td>
+                  <td><?= $d->jumlah_masuk ?></td>
+                </tr>
+              <?php } ?>
+            </tbody>
+          </table>
+          <br>
+          <br>
+          <table align="center" width="800px">
+            <tr>
+              <td width="500px"> </td>
+              <td align="center">
+                Dumai, <?php echo date("d M Y"); ?>
+                <br>
+                <br>
+                <br>
+                <br>
+                ___________________________
+              </td>
+
+            </tr>
+          </table>
+          <p>&nbsp;</p>
+        </td>
+      </tr>
+    </table>
+    <?php
+    echo "<script language=javascript>
+function printWindow() {
+bV = parseInt(navigator.appVersion);
+if (bV >= 4) window.print();}
+printWindow();
+</script>";
+    ?>
+  </form>
+  <br>
+  <br>
+  <br>
 </body>
+
 </html>
